3e44f67e1907d1acac31d0ce98b1045b77be0aa3
Senul Samarasinghe <iwdscs@gmail.com>
2023-09-14 09:41:52 -0400

    Commit 2: changed icon and app colours

--- app/src/main/java/algonquin/cst2335/sama0087/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/sama0087/MainActivity.java
@@ -1,7 +1,10 @@
 package algonquin.cst2335.sama0087;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.appcompat.widget.Toolbar;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +13,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar2);
+
+        // using toolbar as ActionBar
+        setSupportActionBar(toolbar);
     }
 }
