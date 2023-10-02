c3d85b4e69bf11367c5241e3c92fc32cba1a592c
Senul Samarasinghe <iwdscs@gmail.com>
2023-10-02 09:41:52 -0400

    Finished step 2

--- app/src/main/java/algonquin/cst2335/sama0087/ui/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/sama0087/ui/MainActivity.java
@@ -34,60 +34,7 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(variableBinding.getRoot());
 
-        TextView tv = variableBinding.thetextview;
-        Button b = variableBinding.thebutton;
-        EditText et = variableBinding.theeditText;
-        ImageButton ib = variableBinding.theimagebtn;
 
-        variableBinding.thecheckbox.setOnCheckedChangeListener(
-                (btn, onOroff) -> {
-                    vModel.onOroff.postValue(onOroff);
-                });
-
-        variableBinding.theswitch.setOnCheckedChangeListener((btn, onOroff)-> {
-            vModel.onOroff.postValue(onOroff);
-        });
-
-        variableBinding.theradiobutton.setOnCheckedChangeListener((btn, onOroff)-> {
-            vModel.onOroff.postValue(onOroff);
-        });
-
-        ib.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                int width = ib.getWidth();
-                int height = ib.getHeight();
-
-                boolean checkboxState = variableBinding.thecheckbox.isChecked();
-                boolean switchState = variableBinding.theswitch.isChecked();
-                boolean radioButtonState = variableBinding.theradiobutton.isChecked();
-
-                String toastMessage = "Width = " + width + ", Height = " + height +
-                        "\nCheckbox State = " + checkboxState +
-                        "\nSwitch State = " + switchState +
-                        "\nRadio Button State = " + radioButtonState;
-                Toast.makeText(MainActivity.this, toastMessage, Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        vModel.onOroff.observe(this, newValue -> {
-            variableBinding.thecheckbox.setChecked(newValue);
-            variableBinding.theswitch.setChecked(newValue);
-            variableBinding.theradiobutton.setChecked(newValue);
-        });
-
-        et.setText(vModel.userString.getValue());
-
-        vModel.userString.observe(this, (s) -> {
-            b.setText("Your text is now: " + s);
-            tv.setText("Your text is now: " + s);
-        });
-
-        b.setOnClickListener(v -> {
-            vModel.userString.postValue(et.getText().toString());
-        });
-
-        int i = 0;
     }
 }
 
