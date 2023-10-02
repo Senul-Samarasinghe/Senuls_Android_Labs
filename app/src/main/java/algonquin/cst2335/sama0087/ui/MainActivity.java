888e529ba1ff45c7bafede9b05e3cb1879bffdf8
Senul Samarasinghe <iwdscs@gmail.com>
2023-10-02 10:45:31 -0400

    Finished phone version of lab 3

--- app/src/main/java/algonquin/cst2335/sama0087/ui/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/sama0087/ui/MainActivity.java
@@ -8,15 +8,21 @@ import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.RadioGroup;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.os.Bundle;
 import android.widget.Toast;
 
+import algonquin.cst2335.sama0087.R;
 import algonquin.cst2335.sama0087.data.MainViewModel;
 import algonquin.cst2335.sama0087.databinding.ActivityMainBinding;
 
@@ -24,17 +30,31 @@ public class MainActivity extends AppCompatActivity {
 
     ActivityMainBinding variableBinding;
 
-    MainViewModel vModel;
+    ImageView imgView;
+    Switch sw;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        vModel = new ViewModelProvider(this).get(MainViewModel.class);
-        variableBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        imgView = findViewById(R.id.imageView3);
+        sw = findViewById(R.id.spin_switch);
 
-        setContentView(variableBinding.getRoot());
+        sw.setOnCheckedChangeListener( (btn, isChecked) -> {
+            if (isChecked)
+            {
+                RotateAnimation rotate = new RotateAnimation(0, 360, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+                rotate.setDuration(5000);
+                rotate.setRepeatCount(Animation.INFINITE);
+                rotate.setInterpolator(new LinearInterpolator());
 
-
+                imgView.startAnimation(rotate);
+            }
+            else {
+                imgView.clearAnimation();
+            }
+        });
     }
 }
 
