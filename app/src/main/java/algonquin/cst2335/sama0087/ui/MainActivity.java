27599f54b8ac6aea106e73dbf1ca3244e352973a
Senul Samarasinghe <iwdscs@gmail.com>
2023-10-15 12:32:13 -0400

    Finished intents of lab 4

--- app/src/main/java/algonquin/cst2335/sama0087/ui/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/sama0087/ui/MainActivity.java
@@ -6,7 +6,9 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModel;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.LinearInterpolator;
@@ -36,25 +38,51 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        imgView = findViewById(R.id.imageView3);
-        sw = findViewById(R.id.spin_switch);
+        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
 
-        sw.setOnCheckedChangeListener( (btn, isChecked) -> {
-            if (isChecked)
-            {
-                RotateAnimation rotate = new RotateAnimation(0, 360, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
-                rotate.setDuration(5000);
-                rotate.setRepeatCount(Animation.INFINITE);
-                rotate.setInterpolator(new LinearInterpolator());
+        setContentView(binding.getRoot());
 
-                imgView.startAnimation(rotate);
-            }
-            else {
-                imgView.clearAnimation();
-            }
+        //imgView = findViewById(R.id.imageView);
+       // sw = findViewById(R.id.spin_switch);
+
+        binding.loginButton.setOnClickListener( click->{
+            Intent newPage = new Intent(MainActivity.this,SecondActivity.class);
+            String userInput = binding.emailEditText.getText().toString();
+            newPage.putExtra( "EmailAddress", userInput );
+            newPage.putExtra( "Age", 23.450 );
+            startActivity(newPage);
         });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.w( "MainActivity", "In onStart() - Loading Widgets" );
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.w( "MainActivity", "In onPause() - Loading Widgets" );
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.w( "MainActivity", "In onResume() - Loading Widgets" );
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.w( "MainActivity", "In onDestroy() - Loading Widgets" );
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.w( "MainActivity", "In onStop() - Loading Widgets" );
     }
 }
 
