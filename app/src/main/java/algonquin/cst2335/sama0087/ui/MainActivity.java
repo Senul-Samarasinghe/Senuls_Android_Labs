ed8985f93ae7727f8fa4205ba2a92e0ea2b77644
Senul Samarasinghe <iwdscs@gmail.com>
2023-10-30 09:48:57 -0400

    week 5 finished test cases and Javadocs comments

--- app/src/main/java/algonquin/cst2335/sama0087/ui/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/sama0087/ui/MainActivity.java
@@ -1,108 +1,102 @@
 package algonquin.cst2335.sama0087.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModel;
-import androidx.lifecycle.ViewModelProvider;
 
-import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.LinearInterpolator;
-import android.view.animation.RotateAnimation;
 import android.widget.Button;
-import android.widget.CompoundButton;
 import android.widget.EditText;
-import android.widget.ImageButton;
-import android.widget.ImageView;
-import android.widget.RadioGroup;
-import android.widget.Switch;
 import android.widget.TextView;
-import android.os.Bundle;
 import android.widget.Toast;
 
 import algonquin.cst2335.sama0087.R;
-import algonquin.cst2335.sama0087.data.MainViewModel;
-import algonquin.cst2335.sama0087.databinding.ActivityMainBinding;
 
-public class MainActivity extends AppCompatActivity {
+/** Main Activity first screen of the Application
+     * @author Warapitiyage Samarasinghe
+     * @version 0.1
+     */
+    public class MainActivity extends AppCompatActivity {
+        /**
+         * this holds text at center of screen
+         */
+        TextView tv = null;
+        /**
+         * this holds the editable text or password field at the center of screen
+         */
+        EditText et = null;
+        /**
+         * its a button to return result of input of password field
+         */
+        Button btn = null;
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+            tv = findViewById(R.id.textView);
+            et = findViewById(R.id.editText);
+            btn = findViewById(R.id.button);
+            btn.setOnClickListener( clk-> {
+                String password = et.getText().toString();
+                String message = checkPasswordComplexity(password);
+                if (message.isEmpty()){
+                    tv.setText("Your password meets the requirements");
+                } else {
+                    tv.setText("You shall not pass!");
+                }
+            });
+        }
 
-    private EditText emailEditText;
-    private Button loginButton;
-    private SharedPreferences prefs;
-    ActivityMainBinding variableBinding;
-
-    ImageView imgView;
-    Switch sw;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
-
-        setContentView(binding.getRoot());
-
-        //imgView = findViewById(R.id.imageView);
-       // sw = findViewById(R.id.spin_switch);
-
-        emailEditText = findViewById(R.id.emailEditText);
-        loginButton = findViewById(R.id.loginButton);
-
-        // Initialize SharedPreferences
-        prefs = getSharedPreferences("MyData", MODE_PRIVATE);
-
-        // Load the saved email address and set it in the EditText
-        String emailAddress = prefs.getString("LoginName", "");
-        emailEditText.setText(emailAddress);
-
-        loginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Save the email address entered by the user
-                String enteredEmail = emailEditText.getText().toString();
-                SharedPreferences.Editor editor = prefs.edit();
-                editor.putString("LoginName", enteredEmail);
-                editor.apply();
-
-                // Start the SecondActivity
-                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
-                startActivity(intent);
+        /** This function checks for the complexity
+         * of the password entered by user
+         * @param pw The String Object that we are checking
+         * @return returns true if password is complex
+         */
+        String  checkPasswordComplexity( String pw ) {
+            boolean foundUpperCase, foundLowerCase, foundNumber, foundSpecial;
+            foundUpperCase = foundLowerCase = foundNumber = foundSpecial = false;
+            for (int i = 0; i < pw.length(); i++) {
+                char c = pw.charAt(i);
+                if (Character.isDigit(c)){
+                    foundNumber = true;
+                } else if (Character.isUpperCase(c)) {
+                    foundUpperCase = true;
+                } else if (Character.isLowerCase(c)) {
+                    foundLowerCase = true;
+                } else {
+                    foundSpecial = true;
+                }
             }
-        });
-    }
-@Override
-    protected void onStart() {
-        super.onStart();
-        Log.w( "MainActivity", "In onStart() - Loading Widgets" );
-    }
+            if (!foundUpperCase) {
+                return "Missing an uppercase letter";
+            } else if (!foundLowerCase) {
+                return "Missing a lowercase letter";
+            } else if (!foundNumber) {
+                return "Missing a number";
+            } else if (!foundSpecial) {
+                return "Missing a special character";
+            } else {
+                return ""; // Empty string indicates the password meets the requirements
+            }
+        }
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.w( "MainActivity", "In onPause() - Loading Widgets" );
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.w( "MainActivity", "In onResume() - Loading Widgets" );
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.w( "MainActivity", "In onDestroy() - Loading Widgets" );
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.w( "MainActivity", "In onStop() - Loading Widgets" );
-    }
+        /** This function checks for if the provided character
+         * is the Special Character or not
+         * @param c parameter object to be checked
+         * @return Returns true if the provided character is a special character
+         */
+        boolean isSpecialCharacter(char c) {
+            switch (c) {
+                case '#':
+                case '$':
+                case '%':
+                case '^':
+                case '&':
+                case '*':
+                case '!':
+                case '@':
+                case '?':
+                    return true;
+                default:
+                    return false;
+            }
+        }
 }
-
