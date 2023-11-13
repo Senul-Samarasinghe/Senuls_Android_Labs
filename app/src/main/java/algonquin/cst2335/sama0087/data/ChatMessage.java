a73d7004c0bd682d71bf94c3bbded2626838ee59
Senul Samarasinghe <iwdscs@gmail.com>
2023-11-13 07:57:23 -0500

    week 6 finished

---
+++ app/src/main/java/algonquin/cst2335/sama0087/data/ChatMessage.java
@@ -0,0 +1,33 @@
+package algonquin.cst2335.sama0087.data;
+
+public class ChatMessage {
+    String message;
+    String timeSent;
+    boolean isSentButton;
+    public ChatMessage(String m, String t, boolean sent)
+    {
+        message = m;
+        timeSent = t;
+        isSentButton = sent;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setTimeSent(String timeSent) {
+        this.timeSent = timeSent;
+    }
+
+    public String getTimeSent() {
+        return timeSent;
+    }
+
+    public boolean getIsSentButton(){
+        return isSentButton;
+    }
+}
--- app/src/main/java/algonquin/cst2335/sama0087/data/MainViewModel.java
+++ app/src/main/java/algonquin/cst2335/sama0087/data/MainViewModel.java
@@ -3,10 +3,16 @@ package algonquin.cst2335.sama0087.data;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import java.util.ArrayList;
+
 public class MainViewModel extends ViewModel {
 
 
     public MutableLiveData<String> userString = new MutableLiveData("");
 
     public MutableLiveData<Boolean> onOroff = new MutableLiveData<Boolean>(false);
+
+    public class ChatRoomViewModel extends ViewModel {
+        public MutableLiveData<ArrayList<String>> messages = new MutableLiveData<>();
+    }
 }
